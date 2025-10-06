1ce4cc82338a460c7e84a5fd17d7f8cc52a666d4
Gustavo de Freitas Fidelis <gustavo.fidelis@edu.pucrs.br>
2025-10-06 20:24:45 -0300

    ajuste

--- Questao1.java
+++ Questao1.java
@@ -1,10 +1,8 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Questao1 {
     public static void main(String[] args) {
-        int[] ui = {100, 25, 10, 5, 1};
+        int[] ui = {100, 25, 10, 5};
         moedas(ui, 2.89);
     }
 
@@ -15,9 +13,14 @@ public class Questao1 {
         int[] resposta = new int[moedas.length];
 
 
-        for (int i = moedas.length-1; i>=0; i--){
+        for (int i = moedas.length-1; i>0; i--){
             resposta[i] = valorCentavos / moedas[i];
             valorCentavos = valorCentavos % moedas[i];
+        }
+
+        if(valorCentavos > moedas[0]){
+            System.out.println("Não há troco, aceita bala?");
+            return null;
         }
         System.out.println(Arrays.toString(resposta));
         return resposta;
